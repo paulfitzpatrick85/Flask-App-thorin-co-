ff192af276d81d7d9d009d99df12145cb5e8ce0d
paulfitzpatrick85 <fitzer99@gmail.com>
2022-06-22 10:36:14 +0000

    routing with jinja

--- run.py
+++ run.py
@@ -1,21 +1,32 @@
 import os
 # import Flask from flask. capital f Flask is class name
 # saves from having to type html with tags etc
-from flask import Flask, render_template    
+from flask import Flask, render_template 
 
 app = Flask(__name__)    
 
 
 # app.route decorator
-@app.route("/")    # flask what url should trigger function that follows
+# tell flask what url should trigger function that follows,/ goes to top file
+@app.route("/")    
 def index():
     return render_template("index.html")  # will render html page 
 
+
+@app.route("/about")
+def about():
+    return render_template("about.html")  # the view*
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")  # the view*
 
 # create instancce of class. convention in flask is that variable is called app
 # first arg of flask class is the name of the apps module/package
 # single module used so __name__ is used = built in python variable, 
 # flask needs it to look for templates and static files
+
 
 if __name__ == "__main__":
     app.run(
